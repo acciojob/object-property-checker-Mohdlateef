adc2c3d01a237e9bd7f1f1dffc117fe74ed246c5
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-12-07 22:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(key==="red"||key==="green"||key===white)
+	if(key==="red"||key==="green"||key==="white")
 	{
 		return true;
 	}
